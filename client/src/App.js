8b365b326d45a51ef6cac2006b18ee1e88ca8d47
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-09-06 07:36:27 -0300

    Criando rota

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import CadastroUsuario from './pages/CadastroUsuario';
+import CadastrarPerfil from './pages/CadastroPerfil';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path='/cadastro' element={<CadastroUsuario />} />
+          <Route path='/cadastro-perfil' element={<CadastrarPerfil/>}/>
         </Routes>
       </BrowserRouter>
     </>
