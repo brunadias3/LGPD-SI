5f7261647da81df73e6831035536e3b2db801c16
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-10-04 09:32:20 -0300

    feature: trocando class por className

--- client/src/pages/turmas/GerenciarTurmas.js
+++ client/src/pages/turmas/GerenciarTurmas.js
@@ -7,13 +7,13 @@ function GerenciarTrumas() {
             <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl ">
                 <div >
                     <button
-                        class="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
+                        className="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
                         onClick={() => window.location.href = '/cadastro-turma'}
                     >
                         Cadastro de turmas
                     </button>
                     <button
-                        class="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
+                        className="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
                         onClick={() => window.location.href = '/listar-turmas'}
                     >
                         Listar Turmas
--- client/src/pages/usuarios/GerenciarUsuarios.js
+++ client/src/pages/usuarios/GerenciarUsuarios.js
@@ -5,13 +5,13 @@ function GerenciarUsuarios() {
             <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl ">
                 <div >
                     <button
-                        class="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
+                        className="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
                         onClick={() => window.location.href = '/cadastro'}
                     >
                         Cadastro de usuarios
                     </button>
                     <button
-                        class="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
+                        className="bg-gray-900  hover:bg-gray-800 text-white text-2xl font-bold py-2 px-4 rounded-full"
                         onClick={() => window.location.href = '/listar-usuarios'}
                     >
                         Listar usuarios
