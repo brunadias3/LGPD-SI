4826c08a72e50ff0623655325efcbd9b059b31e2
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-10-04 09:30:44 -0300

    feature: retirando alguns itens da navbar e alterando de class para className

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,23 +7,23 @@ function NavBar() {
 
     return (
         <>
-            <nav class=" border-gray-200 bg-gray-900">
+            <nav className=" border-gray-200 bg-gray-900">
 
-                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
                         Gerenciamento escolar
                     </span>
 
-                    <div class="flex items-center md:order-2">
-                        <button type="button" class="flex py-2 pl-3 pr-4 rounded  md:hover:text-blue-900 md:p-0 text-white md:dark:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700" onClick={() => { setIsChecked(true) }}>
+                    <div className="flex items-center md:order-2">
+                        <button type="button" className="flex py-2 pl-3 pr-4 rounded  md:hover:text-blue-900 md:p-0 text-white md:dark:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700" onClick={() => { setIsChecked(true) }}>
                             Ola, nome usuario
                         </button>
                         {/* Perfil do usuário */}
                         {isChecked ?
                             (<div className="nav-item absolute right-1 top-16 bg-gray-700 p-8 rounded-lg w-96 z-50">
                                 <div className="flex  justify-end">
-                                    <button class="font-bold py-2 px-4  hover:bg-gray-900 text-white rounded-full" onClick={() => { setIsChecked(false) }} > x </button>
+                                    <button className="font-bold py-2 px-4  hover:bg-gray-900 text-white rounded-full" onClick={() => { setIsChecked(false) }} > x </button>
                                 </div>
                                 <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
 
@@ -63,18 +63,18 @@ function NavBar() {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" >
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700">
                             <li>
-                                <a className="cursor-pointer block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent  md:p-0 text-white hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700" >Home</a>
+                                <a onClick={() => window.location.href = "/"} className="cursor-pointer block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent  md:p-0 text-white hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700" >Home</a>
                             </li>
-                            <li>
+                            {/* <li>
                                 <a onClick={() => window.location.href = "/gerenciar-usuarios"} className="cursor-pointer block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent  md:p-0 text-white hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Gerenciar Usuários</a>
-                            </li>
+                            </li> */}
                            
                             <li>
                                 <a onClick={() => window.location.href = "/cadastro-perfil"} className= "cursor-pointer block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent  md:p-0 text-white hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Cadastro de perfils</a>
                             </li>
-                            <li>
+                            {/* <li>
                                 <a onClick={() => window.location.href = "/gerenciar-turma"} className= "cursor-pointer block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent  md:p-0 text-white hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">Gerenciamento de turmas</a>
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
                 </div>
